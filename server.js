e6f95e4233da4f7e6fd142556b736d292f61419e
Saetbyeol Lim <slim62@myseneca.ca>
2022-09-21 05:25:19 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Saetbyeol Lim Student ID: 149814212  Date:  
+*
+*  Cyclic Web App URL: 
+*
+*  GitHub Repository URL: 
+*
+********************************************************************************/ 
